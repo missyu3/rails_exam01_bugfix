79e225eaba6a492ceca6e0f202e24a1dd67ae251
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-06-02 15:56:49 +0900

    Fix ブログが削除された時、コメントも削除される

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,9 @@
 class Blog < ApplicationRecord
 # エラー原因: Viewなどで参照している際に複数形で指定されていた
 # 修正の意図: has_manyなので複数形の方が自然なので、こちらを修正
-  has_many :comments
+# エラー原因: ブログが消されても、コメントが残ってしまっている
+# 修正の意図: ゴミデータを残さないためにブログが消されたら紐づくコメントも削除する
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :content, presence: true
