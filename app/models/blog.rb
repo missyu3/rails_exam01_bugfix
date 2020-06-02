9b9f0d67cd60a052401b24443087bcee90dd4623
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-06-02 15:27:55 +0900

    Fix Modelのhas_manyを単数から複数に変更

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-  has_many :comment
+# エラー原因: Viewなどで参照している際に複数形で指定されていた
+# 修正の意図: has_manyなので複数形の方が自然なので、こちらを修正
+  has_many :comments
 
   validates :title, presence: true
   validates :content, presence: true
