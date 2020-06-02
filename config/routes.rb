7bb8b0903ccd7e6047556edea48b73564eaff4f4
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-06-02 15:10:37 +0900

    Fix: routesファイルのスペルミス

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blog do
+# エラー原因: BlogsControllerに対してblogが指定されていたから  
+# 修正の意図: blogsに変更
+  resources :blogs do
     resources :comments, only: [:create, :destroy]
   end
 end
